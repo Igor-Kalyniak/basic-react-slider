21609b366795f7257be5b40bf379341a7f50a8f5
Igor-Kalyniak <igoryok041997@gmail.com>
2018-07-09 00:36:09 +0300

    All page without title

--- src/App.js
+++ src/App.js
@@ -8,29 +8,56 @@ import styled from 'styled-components';
 
 // Bootstrap styles
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
+
+// Import items of components
+import Header from './components/Header';
+import Main from './components/Main';
+import Menu from './components/Menu';
+
+const url = process.env.PUBLIC_URL + './img/main-bg.jpg';
+
+const HeaderWrapper = styled.ul`
+      width: 100%;
+      height: 49px;
+      background-color: #242424;
+      opacity: 0.8;
+`
+
+const MenuWrapper = styled.div`
+      height: 89px;
+      padding-top: 20px;
+`
+
+const MainWrapper = styled.main`
+      height: 600px;
+      padding-top: 130px;
+      background: url(${url});
+      background-size: cover;
+`
 
 // Main React class
 class App extends Component {
   render() {
     return (
-            <div className="wrapper">
-              <SayFullName name="Ihor" surname= "Kalyniak" link= "https://www.linkedin.com/in/igor-kalyniak/" />
-              <SayFullName name="Harry" surname= "Potter" link= "https://github.com/Igor-Kalyniak" />
-              <SayFullName name="Andrew" surname= "Cool" link= "https://www.instagram.com/" />
-            </div>
+        <div className='App'>
+        <HeaderWrapper>
+            <Grid>
+                  <Header />
+            </Grid>
+        </HeaderWrapper>
+        <MenuWrapper>
+            <Grid>
+                  <Menu />
+            </Grid>
+        </MenuWrapper> 
+        <MainWrapper>
+            <Grid>
+                  <Main />
+            </Grid>
+        </MainWrapper>
+        </div>
     );
   }
-}
-
-function SayFullName(props) {
-  return (
-      <div>
-      <h1> My name {props.name}, surname {props.surname}</h1>
-      <a href={props.link}> Profile link </a>
-      </div>
-    )
 }
 
 export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,26 @@
+// Include default React libraries
+import React, { Component } from 'react';
+
+// Include style components
+import styled from 'styled-components';
+
+// Bootstrap styles
+import Row from 'react-bootstrap/lib/Row';
+import Col from 'react-bootstrap/lib/Col';
+
+class Main extends React.Component {
+	render () {
+		return (
+			<Row>
+				<Col lg={5}>
+					
+				</Col>
+				<Col lg={6} lgOffset={1}>
+
+				</Col>
+			</Row>
+			)	
+	}
+}
+
+export default Main
